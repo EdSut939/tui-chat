7fb093753036fa93508d74492d5d76a9deeca961
Edvinas Sutkus <sutkuse@gmail.com>
2026-07-29 19:56:52 +0300

    handling multiple clients

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
@@ -13,29 +14,55 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan []byte)
+var mutex = &sync.Mutex{}
+
 func wsHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println("Error upgrading:", err)
 		return
 	}
+
 	defer conn.Close()
+
+	mutex.Lock()
+	clients[conn] = true
+	mutex.Unlock()
+
 	for {
 		_, message, err := conn.ReadMessage()
 		if err != nil {
 			fmt.Println("Error reading message:", err)
+			mutex.Lock()
+			delete(clients, conn)
+			mutex.Unlock()
 			break
 		}
-		fmt.Printf("Received: %s\\n", message)
-		if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
-			fmt.Println("Error writing message:", err)
-			break
+		broadcast <- message
+	}
+}
+
+func handleMessages() {
+	for {
+		message := <-broadcast
+
+		mutex.Lock()
+		for client := range clients {
+			err := client.WriteMessage(websocket.TextMessage, message)
+			if err != nil {
+				client.Close()
+				delete(clients, client)
+			}
 		}
+		mutex.Unlock()
 	}
 }
 
 func main() {
 	http.HandleFunc("/ws", wsHandler)
+	go handleMessages()
 	fmt.Println("WebSocket server started on :8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
